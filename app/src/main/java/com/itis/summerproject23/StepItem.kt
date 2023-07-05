f971e32092080c8edaeadab6e10890f7ae7baea6
Timur Medov <fyvakek@gmail.com>
2023-07-06 02:20:16 +0300

    add ability to write ingredients

--- app/src/main/java/com/itis/summerproject23/StepItem.kt
+++ app/src/main/java/com/itis/summerproject23/StepItem.kt
@@ -9,21 +9,26 @@ import com.itis.summerproject23.databinding.ItemStepBinding
 class StepItem(
     private val binding: ItemStepBinding,
     private val glide: RequestManager,
-) : RecyclerView.ViewHolder(binding.root)  {
+) : RecyclerView.ViewHolder(binding.root) {
 
-    private val options: RequestOptions = RequestOptions()
-        .diskCacheStrategy(DiskCacheStrategy.ALL)
+    private val options: RequestOptions = RequestOptions().diskCacheStrategy(DiskCacheStrategy.ALL)
 
-    fun onBind(step : Step) {
+    fun onBind(step: Step) {
         binding.run {
-            tvTitle.text = String.format(R.string.step_id.toString(), step.id)
+            if (step.id == 0) {
+                tvTitle.text = R.string.ingredients.toString()
+                glide.load(R.drawable.empty_img)
+                    .into(ivImage)
+            } else {
+                tvTitle.text = String.format(R.string.step_id.toString(), step.id)
+                glide.load(step.img_url)
+                    .placeholder(R.drawable.loading)
+                    .circleCrop()
+                    .apply(options)
+                    .error(R.drawable.image_not_available)
+                    .into(ivImage)
+            }
             tvDesc.text = step.text
-
-            glide.load(step.img_url)
-                .placeholder(R.drawable.loading)
-                .apply(options)
-                .error(R.drawable.image_not_available)
-                .into(ivImage)
         }
     }
 }
