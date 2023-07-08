2db48280475b989893ab4604fe8c53bcb8f37883
Timur Medov <fyvakek@gmail.com>
2023-07-08 20:30:52 +0300

    rebase

--- app/src/main/java/com/itis/summerproject23/UserAcoountFragment.kt
+++ app/src/main/java/com/itis/summerproject23/UserAcoountFragment.kt
@@ -7,7 +7,7 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 
-class UserAccountFragment : Fragment(R.layout.fragment_user_account) {
+class UserAcoountFragment : Fragment(R.layout.fragment_user_account) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
